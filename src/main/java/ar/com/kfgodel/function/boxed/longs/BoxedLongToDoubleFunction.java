3a29e1a16667f9220104e70ceb7911b8744c3c1e
Dario L Garcia <dario.garcia@10pines.com>
2017-08-06 13:49:18 -0300

    Add Long to primitive number primitons

--- src/main/java/ar/com/kfgodel/function/boxed/longs/BoxedLongToDoubleFunction.java
+++ src/main/java/ar/com/kfgodel/function/boxed/longs/BoxedLongToDoubleFunction.java
@@ -1,10 +1,10 @@
 package ar.com.kfgodel.function.boxed.longs;
 
-import ar.com.kfgodel.function.objects.ObjectToFloatFunction;
+import ar.com.kfgodel.function.objects.ObjectToDoubleFunction;
 
 /**
  * Date: 29/07/17 - 19:57
  */
-public interface BoxedLongToDoubleFunction extends ObjectToFloatFunction<Long> {
+public interface BoxedLongToDoubleFunction extends ObjectToDoubleFunction<Long> {
 
 }
--- src/main/java/ar/com/kfgodel/function/boxed/shorts/BoxedShortToDoubleFunction.java
+++ src/main/java/ar/com/kfgodel/function/boxed/shorts/BoxedShortToDoubleFunction.java
@@ -1,10 +1,10 @@
 package ar.com.kfgodel.function.boxed.shorts;
 
-import ar.com.kfgodel.function.objects.ObjectToFloatFunction;
+import ar.com.kfgodel.function.objects.ObjectToDoubleFunction;
 
 /**
  * Date: 29/07/17 - 19:57
  */
-public interface BoxedShortToDoubleFunction extends ObjectToFloatFunction<Short> {
+public interface BoxedShortToDoubleFunction extends ObjectToDoubleFunction<Short> {
 
 }
