35138b25c22a86b5a7e79b3a917244b85cce203f
Dario L Garcia <dario.garcia@10pines.com>
2017-08-06 13:21:54 -0300

    Add float to primitive number primitons

--- src/main/java/ar/com/kfgodel/function/boxed/floats/BoxedFloatToDoubleFunction.java
+++ src/main/java/ar/com/kfgodel/function/boxed/floats/BoxedFloatToDoubleFunction.java
@@ -1,10 +1,10 @@
 package ar.com.kfgodel.function.boxed.floats;
 
-import ar.com.kfgodel.function.objects.ObjectToFloatFunction;
+import ar.com.kfgodel.function.objects.ObjectToDoubleFunction;
 
 /**
  * Date: 29/07/17 - 19:57
  */
-public interface BoxedFloatToDoubleFunction extends ObjectToFloatFunction<Float> {
+public interface BoxedFloatToDoubleFunction extends ObjectToDoubleFunction<Float> {
 
 }
